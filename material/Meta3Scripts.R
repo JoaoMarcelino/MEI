214ffd49025d8bb057952e32892dc8e1c088c5d4
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-12-21 15:02:21 +0000

    anova testing

--- material/Meta3Scripts.R
+++ material/Meta3Scripts.R
@@ -1,6 +1,8 @@
 library(gplots)
 library(ggplot2)
 library(ggpubr)
+library(tidyverse)
+library(rstatix)
 
 
 #setwd Marcelino
@@ -29,36 +31,112 @@ library(ggpubr)
 #show(p)
 
 #Teste 3 -----------------------------------------------------------------------
-table=read.table("./results_2/test3.txt",header = TRUE)
-table1 = subset(table, select=c(nExams,time, codigo, overlapProb))
+table=read.table("./results_2/test4.txt",header = TRUE)
+table1 = subset(table,select=c(nExams,time, codigo, overlapProb))
 
  
  
 table1$codigo <- factor(table1$codigo, 
                        levels = c( 1, 2),
                        labels = c("C1", "C2"))
+
+table1$nExams <- factor(table1$nExams,
+                        levels = c( 5, 10, 15, 20),
+                        labels = c("n5", "n10", "n15", "n20"))
+
+
+table1$overlapProb <- factor(table1$overlapProb,
+                        levels = c( 0.1, 0.3, .5, 0.7, 0.9),
+                        labels = c("p10", "p30", "p50", "p70", "p90"))
  
 
-p = ggboxplot(table1, x = "nExams", y = "time", pallete = "overlapProb", facet.by = "codigo")
-
+bxp <- ggboxplot(
+  table1, x = "overlapProb", y = "time",
+  color = "nExams", facet.by = "codigo", ylim = c(0,1))
 
 
 model  <- lm(time ~ codigo*nExams*overlapProb, data = table1)
 # Create a QQ plot of residuals
-q = ggqqplot(residuals(model))
+qq = ggqqplot(residuals(model), ggtheme = theme_bw())
 
+show(shapiro_test(residuals(model)))
 
-
-r = ggqqplot(table1, "time", ggtheme = theme_bw()) +
+r = ggqqplot(table1,"time",ggtheme = theme_bw(),) + 
   facet_grid(codigo + nExams ~ overlapProb, labeller = "label_both")
 
 
 
+three.way = aov(time ~ codigo*nExams*overlapProb, data = table1)
+
+show(summary(three.way))
+
 res.aov <- table1 %>% anova_test(time ~ codigo*nExams*overlapProb)
 show(res.aov)
-
 
 # https://www.datanovia.com/en/lessons/anova-in-r/#computation-2
 # Post-hoc tests
 
-show(r)
+show(qq)
+
+
+show("Two Way")
+
+
+#POST HOC
+# simple two-way interactions
+
+model  <- lm(time ~ codigo*nExams*overlapProb, data = table1)
+groupedBy <- table1 %>%
+  group_by(overlapProb) %>%
+  anova_test(time ~ nExams*codigo, error = model)
+
+show(groupedBy)
+
+
+#simple simple main effects
+pp <-table1 %>%group_by(overlapProb) %>%group_by(overlapProb, codigo) %>%anova_test(time ~ nExams, error = model)
+
+show(pp)
+
+# # Group the data by gender and risk, and fit  anova
+# treatment.effect <- table1 %>%
+#   group_by(codigo, overlapProb) %>%
+#   anova_test(time ~ nExams, error = model)
+# treatment.effect %>% filter(codigo == "c1")
+
+
+
+
+
+
+# Two Way anova between nExams and overlapProb
+# 
+# table2 = subset(table, select=c(nExams,time, overlapProb))
+# 
+# table2$nExams <- factor(table2$nExams,
+#                         levels = c( 5, 10, 15, 20),
+#                         labels = c("n5", "n10", "n15", "n20"))
+# 
+# bxp2 <- ggboxplot(
+#   table2, x = "overlapProb", y = "time",
+#   color = "nExams")
+# 
+# show(bxp2)
+# 
+# 
+# # Build the linear model
+# 
+# model  <- lm(time ~ nExams * overlapProb,
+#              data = table2)
+# 
+# #Create a QQ plot of residuals
+# qqplot <- ggqqplot(residuals(model))
+# 
+# goodplot <- ggqqplot(table2, "time", ggtheme = theme_bw()) +
+#   facet_grid(nExams ~ overlapProb)
+# 
+# show(goodplot)
+# 
+# res.aov <- table2 %>% anova_test(time ~ nExams * overlapProb)
+# show(res.aov)
+
